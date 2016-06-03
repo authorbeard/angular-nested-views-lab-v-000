6c7868e97237356bac15dab1fcca3cd51067e727
authorbeard <msnolan@gmail.com>
2016-06-03 08:09:04 -0400

    added .otherwise

--- js/app/app.js
+++ js/app/app.js
@@ -1,6 +1,6 @@
 angular
     .module('app', ['ui.router'])
-    .config(function ($stateProvider){
+    .config(function ($stateProvider, $urlRouterProvider){
         $stateProvider
             .state('home', {
                 url: '/',
@@ -21,5 +21,7 @@ angular
                 url: 'user',
                 templateUrl: 'views/home/user.html'
             })
+
+        $urlRouterProvider.otherwise('/')
 
     })
